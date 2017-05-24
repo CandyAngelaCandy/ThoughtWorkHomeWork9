c0734ed7c431dd48bb6a851df9c5550e29691e78
Candy <229355694@qq.com>
2017-05-24 16:59:54 +0800

    second commit

--- main/main.js
+++ main/main.js
@@ -1,4 +1,31 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(str) {
+  var result = numInverToLcd(str);
+  return result;
 };
+
+function numInverToLcd(str) {
+  var strArr = str.split("").map(function (item) {
+    return +item;
+  });
+
+  var numStr = [
+    ["._.", "|.|", "|_|"], ["...", "..|", "..|"], ["._.", "._|", "|_."],
+    ["._.", "._|", "._|"], ["...", "|_|", "..|"], ["._.", "|_.", "._|"],
+    ["._.", "|_.", "|_|"], ["._.", "..|", "..|"], ["._.", "|_|", "|_|"],
+    ["._.", "|_|", "..|"]
+  ];
+
+  var arr = [];
+  for (var i = 0; i < 3; i++) {
+    var temp = [];
+    for (var j = 0, len = strArr.length; j < len; j++) {
+      var index = strArr[j];
+      temp.push(numStr[index][i]);
+    }
+    arr.push(temp.join(" "));
+  }
+  var result = arr.join("\n") + "\n";
+  return result;
+}
+
+
